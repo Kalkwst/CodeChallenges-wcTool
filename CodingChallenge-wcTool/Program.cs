fd863c18576bdbf90b51f3aa20e37f6903e08dfc
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-10-18 20:05:48 +0300

    Add help and character count

--- CodingChallenge-wcTool/Program.cs
+++ CodingChallenge-wcTool/Program.cs
@@ -3,123 +3,160 @@ using CommandLine;
 
 class Program
 {
-    private static uint totalLines;
-    private static uint totalWords;
-    private static uint totalChars;
-    private static uint totalBytes;
+    private static uint _totalLines;
+    private static uint _totalWords;
+    private static uint _totalChars;
+    private static uint _totalBytes;
 
-    private static string data = string.Empty;
+    private static string _data = string.Empty;
 
-    private static bool printLines;
-    private static bool printWords;
-    private static bool printChars;
-    private static bool printBytes;
+    private static bool _printLines;
+    private static bool _printWords;
+    private static bool _printChars;
+    private static bool _printBytes;
 
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         Parser.Default.ParseArguments<Options>(args)
             .WithParsed<Options>(opts => RunOptions(opts))
             .WithNotParsed<Options>((errs) => HandleParseError(errs));
     }
 
-    static void RunOptions(Options opts)
+    private static void RunOptions(Options opts)
     {
+        if (opts.showHelp)
+        {
+            DisplayHelp();
+            return;
+        }
+        
         if (opts.countBytes)
         {
-            printBytes = true;
+            _printBytes = true;
         }
 
         if (opts.countChars)
         {
-            printChars = true;
+            _printChars = true;
         }
 
         if (opts.countLines)
         {
-            printLines = true;
+            _printLines = true;
         }
 
         if (opts.countWords)
         {
-            printWords = true;
+            _printWords = true;
         }
 
         if (!string.IsNullOrEmpty(opts.FilePath))
         {
-            data = File.ReadAllText(opts.FilePath);
+            _data = File.ReadAllText(opts.FilePath);
         }
         else
         {
             while (Console.ReadLine()! is { } line)
             {
-                data += line + "\r\n";
+                _data += line + "\r\n";
             }
         }
 
-        if (!(printBytes || printChars || printLines || printWords))
+        if (!(_printBytes || _printChars || _printLines || _printWords))
         {
-            printBytes = true;
-            printLines = true;
-            printWords = true;
+            _printBytes = true;
+            _printLines = true;
+            _printWords = true;
         }
 
-        if (printBytes)
+        if (_printBytes)
         {
-            GetByteCount(data);
+            GetByteCount(_data);
         }
 
-        if (printLines)
+        if (_printChars)
         {
-            GetLineCount(data);
+            GetCharacterCount(_data);
         }
 
-        if (printWords)
+        if (_printLines)
         {
-            GetWordCount(data);
+            GetLineCount(_data);
         }
 
-        WriteCounts(totalLines, totalWords, totalChars, totalBytes, opts.FilePath);
+        if (_printWords)
+        {
+            GetWordCount(_data);
+        }
+
+        WriteCounts(_totalLines, _totalWords, _totalChars, _totalBytes, opts.FilePath);
     }
 
-    static void HandleParseError(IEnumerable<Error> errs)
+    private static void HandleParseError(IEnumerable<Error> errs)
     {
     }
 
-    static void GetByteCount(string data)
+    private static void GetByteCount(string data)
     {
-        totalBytes += (uint)Encoding.Default.GetByteCount(data);
+        _totalBytes += (uint)Encoding.Default.GetByteCount(data);
     }
 
-    static void GetLineCount(string data)
+    private static void GetCharacterCount(string data)
     {
-        totalLines += (uint)data.Split(new string[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries)
+        _totalChars += (uint)data.Length;
+    }
+
+    private static void GetLineCount(string data)
+    {
+        _totalLines += (uint)data.Split(new string[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries)
             .Length;
     }
 
-    static void GetWordCount(string data)
+    private static void GetWordCount(string data)
     {
-        totalWords += (uint)data.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+        _totalWords += (uint)data.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
             .Length -1;
+    }
+
+    private static void DisplayHelp()
+    {
+        var programName = "wc";
+        Console.WriteLine("Usage: [OPTION] [FILE]");
+        Console.WriteLine("Print newline, word, and byte counts for the FILE. A word is a nonempty sequence of non white");
+        Console.WriteLine("space delimited by white space characters or by start or end of input.");
+        Console.WriteLine("With no FILE, read standard input.");
+        Console.WriteLine("The options below may be used to select witch counts are printed, always in");
+        Console.WriteLine("the following order: newline, word, character, byte.");
+        Console.WriteLine("  -c, --bytes            print the byte counts");
+        Console.WriteLine("  -m, --chars            print the character counts");
+        Console.WriteLine("  -l, --lines            print the newline counts");
+        Console.WriteLine("  -w, --words            print the word counts");
+        Console.WriteLine("  -h, --help             display this help and exit");
     }
 
     private static void WriteCounts(uint lines, uint words, uint chars, uint bytes, string file)
     {
-        if (printBytes)
+        if (_printBytes)
         {
             Console.Write($"{bytes} ");
         }
+
+        if (_printChars)
+        {
+            Console.Write($"{chars}");
+        }
         
-        if (printLines)
+        if (_printLines)
         {
             Console.Write($"{lines} ");
         }
 
-        if (printWords)
+        if (_printWords)
         {
             Console.Write($"{words} ");
         }
 
-        if (printChars)
+        if (_printChars)
         {
             Console.Write($"{chars} ");
         }
@@ -146,6 +183,9 @@ class Options
     
     [Option('w', "words", Required = false, HelpText = "Print the word counts")]
     public bool countWords { get; set; }
+    
+    [Option('h', "help", Required = false, HelpText = "Display the manual")]
+    public bool showHelp { get; set; }
     
     [Value(0, Required = false, HelpText = "The file to process")]
     public string FilePath { get; set; }
